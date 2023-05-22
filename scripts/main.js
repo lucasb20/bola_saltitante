63dd3db9b5735644e0eddbab411c8373dd4e0bff
Lucas Rocha <lr359508@gmail.com>
2023-05-22 15:07:17 -0300

    Ajustando o Canvas.

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,9 @@ ctx = canvas.getContext('2d')
 
 ctx.beginPath()
 ctx.fillStyle = "#ff0000"
-ctx.arc(100,60,50,0,2*Math.PI,false)
+ctx.arc(150,75,10,0,2*Math.PI,false)
 ctx.fill()
 
-ctx.lineWidth = 10
-ctx.strokeRect(0,0,200,200)
+ctx.lineWidth = 5
+ctx.strokeRect(0,0,300,150)
+
