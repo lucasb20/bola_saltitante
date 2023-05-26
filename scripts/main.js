6b5b0747dc6c155aa2d5b797a381ce0529f6c4f2
Lucas Rocha <lr359508@gmail.com>
2023-05-26 13:52:38 -0300

    Correções lógicas e visuais.
    Botão de mudar velocidade da bola agora funciona de verdade, e a bola é ricocheteia após colidir na borda preta em vez de atravessar ela e ricocheteiar.

--- scripts/main.js
+++ scripts/main.js
@@ -4,28 +4,30 @@ canvas.style.background = "darkolivegreen"
 
 ctx = canvas.getContext('2d')
 
+const lineWidth = 5
+
 let ballx=150,bally=75,ballrad=2
 let ballvx=4,ballvy=8
 let boxx=0,boxy=0,box_tamx=300,box_tamy=150
-let boxboundR=box_tamx+boxx-ballrad
-let boxboundB=box_tamy+boxy-ballrad
-let boxboundL=boxx+ballrad
-let boxboundT=boxy+ballrad
+let boxboundR=box_tamx+boxx-lineWidth
+let boxboundB=box_tamy+boxy-lineWidth
+let boxboundL=boxx+lineWidth
+let boxboundT=boxy+lineWidth
 
 let button = document.querySelector('#but_change')
 button.addEventListener('click',change)
-let new_vx = Number((document.getElementsByTagName('input')[0]).value)
-let new_vy = Number((document.getElementsByTagName('input')[1]).value)
 
-ctx.beginPath()
-ctx.fillStyle = "rgb(255,0,0)"
-ctx.arc(ballx,bally,ballrad,0,2*Math.PI,false)
-ctx.fill()
-
-ctx.lineWidth = ballrad
-ctx.strokeRect(boxx,boxy,box_tamx,box_tamy)
+let new_vx,new_vy
 
 function init(){
+    ctx.beginPath()
+    ctx.fillStyle = "rgb(255,0,0)"
+    ctx.arc(ballx,bally,ballrad,0,2*Math.PI,false)
+    ctx.fill()
+
+    ctx.lineWidth = lineWidth
+    ctx.strokeRect(boxx,boxy,box_tamx,box_tamy)
+
     move_ball()
     setInterval(move_ball,50)
 }
@@ -63,6 +65,20 @@ function move_and_check(){
 }
 
 function change(){
-    ballvx = new_vx
-    ballvy = new_vy
+    new_vx = Number((document.getElementsByTagName('input')[0]).value)
+    new_vy = Number((document.getElementsByTagName('input')[1]).value)
+    
+    if(new_vx*ballvx>=0){
+        ballvx = new_vx
+    }
+    else{
+        ballvx = -new_vx
+    }
+
+    if(new_vy*ballvy>=0){
+        ballvy = new_vy
+    }
+    else{
+        ballvy = -new_vy
+    }
 }
